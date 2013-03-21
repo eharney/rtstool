1ccabc21be63fd72f7fc9c8e2085ba8ba65099fe
Eric Harney <eharney@redhat.com>
2013-03-21 16:47:25 -0400

    Add license text to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
 
+# Copyright 2013 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, version 3 (AGPLv3).
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
 from distutils.core import setup
 
 setup(name='rtstool',
-      version='0.1a2',
+      version='0.1a3',
       description='rtslib command-line interface',
       author='Eric Harney',
       author_email='eharney@redhat.com',
